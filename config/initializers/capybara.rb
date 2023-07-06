a9ad25462ded9d51657fb6b0c704dc607d6c518c
Nicky Dover <ndover@bonobos.com>
2023-07-06 15:58:36 -0400

    increase timeout on selenium driver

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,1 +1,9 @@
-Capybara.default_driver = :selenium_chrome_headless # :selenium_chrome
+Capybara.register_driver :chrome_timeout do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.read_timeout = 120
+  Capybara::Selenium::Driver.new(app, :browser => :chrome, :http_client => client, :options => options)
+end
+
+Capybara.default_driver = :chrome_timeout
