0ccf25a9c06cb5f5a1ceb605ba861b372cb749e1
Nicky Dover <ndover@bonobos.com>
2023-07-07 13:21:26 -0400

    add drafts, cans, and beers endpoint

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,10 @@
 class BeersController < ApplicationController
+
+  def beers
+    beers = UntappdScraperService.new(permitted_params).full_menu.merge(HoptimistScraperService.new(permitted_params).menu)
+    render json: beers
+  end
+
   def drafts
     render json: UntappdScraperService.new(permitted_params).full_menu
   end
